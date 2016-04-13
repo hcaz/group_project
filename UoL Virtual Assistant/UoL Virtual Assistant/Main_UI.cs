15e265d9f27a353b2c4be1b7630fc790ce2dd5e5
Zachary Claret-Scott <github@hcaz.me.uk>
2016-04-13 16:43:45 +0100

    Added remembering of last message

--- UoL Virtual Assistant/UoL Virtual Assistant/Main_UI.cs
+++ UoL Virtual Assistant/UoL Virtual Assistant/Main_UI.cs
@@ -34,6 +34,10 @@ namespace UoL_Virtual_Assistant
         public static string Latest_User_Message = ""; //this is a string that contains the latest user message. it is easily accessable from other areas of the system
         public static string Latest_AI_Message = ""; // ....
         public static XmlNode currentObject;
+        public static bool waitingOnResponse = false;
+        public static string waitingOnResponsePos;
+        public static string waitingOnResponseNeg;
+
 
         int R; int G; int B; //creates R,G,B values for themes
         int Open_Settings_Drawer = 0; //a value of 0 indicates that the drawer is shut
