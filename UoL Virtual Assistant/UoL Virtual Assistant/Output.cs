a3dce04f139caf59bc7af28a6b8d691893da997f
Zachary Claret-Scott <github@hcaz.me.uk>
2016-04-27 13:31:12 +0100

    Added group chat [BROKEN]

--- UoL Virtual Assistant/UoL Virtual Assistant/Output.cs
+++ UoL Virtual Assistant/UoL Virtual Assistant/Output.cs
@@ -81,6 +81,7 @@ namespace UoL_Virtual_Assistant
                         else if (Main_UI.Latest_User_Message.ToLower().Contains("1 + 1"))
                         {
                             output = "Hold on a moment, let me invite JB";
+                            Add_Agent_To_Conversation(2);
                         }
                         else if (currentSentance.sentenceType.Contains(ContextObject.SentenceType.insult))
                         {
